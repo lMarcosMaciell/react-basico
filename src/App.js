8b9ef25220657ec330f6b325ab48257a8e6274bb
lMarcosMaciell <marcos.maciel1899@gmail.com>
2024-04-20 11:37:12 -0300

    Manipulando o estado da aplicação

--- src/App.js
+++ src/App.js
@@ -1,15 +1,67 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Comentario from './components/Comentario.js';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Meu Projeto</h1>
-      <Comentario/>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    comentarios: [
+      {
+        nome: 'João',
+        email: 'joão@mail.com',
+        data: new Date(2024, 4, 20),
+        mensagem: 'Olá, tudo bem ?'
+      },
+      {
+        nome: 'Pedro',
+        email: 'pedro@mail.com',
+        data: new Date(2024, 4, 19),
+        mensagem: 'Olá, tudo bem sim...'
+      }
+    ]
+  }
+
+  adicionarComentario = () => {
+    console.log("Adicionando comentário");
+
+    const novoComentario = {
+      nome: 'Marcos',
+      email: 'marcos@mail.com',
+      data: new Date(),
+      mensagem: 'Olá pessoal !'
+    }
+
+    let lista = this.state.comentarios;
+    lista.push(novoComentario);
+
+
+    this.setState({comentarios: [...this.state.comentarios, novoComentario]})
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Meu Projeto</h1>
+
+        {this.state.comentarios.map((comentario, indice) => (        
+
+        <Comentario 
+        key={indice}
+        nome={comentario.nome}
+        email={comentario.email}
+        data={comentario.data}>
+          {comentario.mensagem}
+          </Comentario>
+  ))}
+
+      <button onClick={this.adicionarComentario}>Adicionar um Comentário</button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Comentario.css'
 
-const Comentario = () => (
-    <div className="Comentario">
-        <div>João:</div>
-        <div>Olá, tudo bem ?</div>
+const Comentario = props => {
+   
+   
+   return <div className="Comentario">
+        <h2>{props.nome}</h2>
+        <p>{props.email}</p>
+        <p>{props.children}</p>
+        <p>{props.data.toString()}</p>
     </div>
-);
+};
 
 export default Comentario
