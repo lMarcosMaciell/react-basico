321443cb3e536880be82157db40d324eacd17296
lMarcosMaciell <marcos.maciel1899@gmail.com>
2024-04-21 12:06:57 -0300

    Interação Entre Componentes

--- src/App.js
+++ src/App.js
@@ -46,6 +46,12 @@ class App extends Component {
 
   }
 
+  removerComentario = comentario => {
+    let lista = this.state.comentarios;
+    lista = lista.filter(c => c !== comentario)
+    this.setState({ comentarios: lista})
+  }
+
   digitacao = evento => {
     const {name, value} = evento.target;
       this.setState({ novoComentario: {...this.state.novoComentario, [name]: value }})
@@ -63,7 +69,8 @@ class App extends Component {
         key={indice}
         nome={comentario.nome}
         email={comentario.email}
-        data={comentario.data}>
+        data={comentario.data}
+        onRemove={this.removerComentario.bind(this, comentario)}>
           {comentario.mensagem}
           </Comentario>
   ))}
